324e37e9b5282bd8292131d7e6d0c114a3ac1ad1
salvanabavym-ux <salvanabavym@gmail.com>
2026-05-16 14:29:36 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,261 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-panorama-book-store-1",
+      "name": "Panorama book store 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "rotation": 1.5707963267948966,
+          "target": "4-panorama-bookstore-2"
+        },
+        {
+          "yaw": -3.1338476575722396,
+          "pitch": 0.11263593172713016,
+          "rotation": 4.71238898038469,
+          "target": "1-panorama-book-store-4"
+        },
+        {
+          "yaw": 1.554204062149192,
+          "pitch": -0.4368503748272694,
+          "rotation": 0,
+          "target": "0-panorama-book-store-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-panorama-book-store-4",
+      "name": "Panorama book store 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.37594637189851277,
+          "pitch": 0.1656054769672597,
+          "rotation": 1.5707963267948966,
+          "target": "0-panorama-book-store-1"
+        },
+        {
+          "yaw": 1.7173609386631359,
+          "pitch": -0.34576714110295015,
+          "rotation": 0,
+          "target": "2-panorama-bookstore-5"
+        },
+        {
+          "yaw": -2.9590332964879646,
+          "pitch": 0.10999092656363452,
+          "rotation": 4.71238898038469,
+          "target": "3-panorama-book-store-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-panorama-bookstore-5",
+      "name": "Panorama bookstore 5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.030394655296934303,
+          "pitch": 0.42130700573579993,
+          "rotation": 0,
+          "target": "0-panorama-book-store-1"
+        },
+        {
+          "yaw": 0.9623751572618122,
+          "pitch": 0.5942257611464967,
+          "rotation": 1.5707963267948966,
+          "target": "4-panorama-bookstore-2"
+        },
+        {
+          "yaw": -0.898025913507265,
+          "pitch": 0.7107870095756539,
+          "rotation": 4.71238898038469,
+          "target": "1-panorama-book-store-4"
+        },
+        {
+          "yaw": 3.115499205707078,
+          "pitch": 0.6241173221243113,
+          "rotation": 0,
+          "target": "3-panorama-book-store-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-panorama-book-store-3",
+      "name": "Panorama book store 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "rotation": 1.5707963267948966,
+          "target": "1-panorama-book-store-4"
+        },
+        {
+          "yaw": 1.3548348738322744,
+          "pitch": -0.2831902119923093,
+          "rotation": 0,
+          "target": "2-panorama-bookstore-5"
+        },
+        {
+          "yaw": -3.089444258785319,
+          "pitch": 0.08062529238370963,
+          "rotation": 10.995574287564278,
+          "target": "4-panorama-bookstore-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-panorama-bookstore-2",
+      "name": "Panorama bookstore 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2412658079761023,
+          "pitch": 0.09276244657671029,
+          "rotation": 1.5707963267948966,
+          "target": "3-panorama-book-store-3"
+        },
+        {
+          "yaw": 2.969894492362643,
+          "pitch": 0.07351055686627816,
+          "rotation": 10.995574287564278,
+          "target": "0-panorama-book-store-1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "retail book shop 360",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
